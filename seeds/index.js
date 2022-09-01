438f4c0bceef7d123e17977843ce61a4674934e3
deepshika <sharmadeepshika22@gmail.com>
2022-09-02 06:37:47 +1200

    changed folder structure

---
+++ seeds/index.js
@@ -0,0 +1,38 @@
+// We'll run this everytime we want to seed the database
+
+const Campground = require("../models/campground");
+const cities = require("./cities");
+const { places, descriptors } = require("./seedHelpers");
+
+// MONGOOSE
+const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://localhost:27017/yelp-camp")
+  .then(() => {
+    console.log("MONGOOSE CONNECTION OPEN!!");
+  })
+  .catch((error) => {
+    console.log("MONGOOSE CONNECTIO ERROR!!");
+    console.log(error);
+  });
+
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const camp = new Campground({
+      title: `${sample(descriptors)} ${sample(places)}`,
+      price: `${random1000}`,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      location: `${cities[random1000]["city"]}, ${cities[random1000]["state"]}`,
+    });
+    await camp.save();
+  }
+};
+
+seedDB().then(() => {
+  mongoose.connection.close();
+});
