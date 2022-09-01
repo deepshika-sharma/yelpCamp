0184d47b6947ad2edfb23f1596fc87f32849380e
deepshika <sharmadeepshika22@gmail.com>
2022-09-02 10:28:05 +1200

    added crud

--- app.js
+++ app.js
@@ -23,20 +23,65 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
+// CRUD
+// HOME PAGE -- ALL CAMPGROUNDS
 app.get("/", (req, res) => {
-  res.render("home");
+  res.redirect("/campgrounds");
 });
 
-app.get("/makecampground", async (req, res) => {
-  const camp = new Campground({
-    title: "Beverly Hills",
-    price: "$499",
-    description: "lorem;oeffhgjkrhjkhjkhjkfg",
-    location: "Sigatoka, Fj",
-  });
-  await camp.save();
-  res.send(camp);
+app.get("/campgrounds", async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render("allCampgrounds", { campgrounds });
 });
+
+// ADDING A NEW CAMPGROUND
+app.get("/campgrounds/new", (req, res) => {
+  res.render("newCampground");
+});
+
+app.post("/campgrounds", async (req, res) => {
+  console.log(req.body);
+  await Campground.insertMany(req.body);
+  res.redirect("/campgrounds");
+});
+
+// VIEWING A CAMPGROUND
+app.get("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  res.render("campground", { campground });
+});
+
+// EDIT A CAMPGROUND
+app.get("/campgrounds/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  res.render("edit", { campground });
+});
+
+app.patch("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  await Campground.findByIdAndUpdate(id, req.body);
+  res.redirect(`/campgrounds/${id}`);
+});
+
+// DELETE A CAMPGROUND
+app.delete("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  await Campground.findByIdAndDelete(id);
+  res.redirect("/campgrounds");
+});
+
+// app.get("/makecampground", async (req, res) => {
+//   const camp = new Campground({
+//     title: "Beverly Hills",
+//     price: "$499",
+//     description: "lorem;oeffhgjkrhjkhjkhjkfg",
+//     location: "Sigatoka, Fj",
+//   });
+//   await camp.save();
+//   res.send(camp);
+// });
 
 app.listen(3000, () => {
   console.log("LISTENING ON PORT 3000!");
